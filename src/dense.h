0fc37b1211fae75912f6db472507244da3d05e3e
Bao Pham <baopham4998@gmail.com>
2022-04-14 21:44:29 -0400

    Changes to Matrix Library and Layer Library

--- src/dense.h
+++ src/dense.h
@@ -4,36 +4,67 @@
 #include "layer.h"
 
 using std::string;
-
 typedef Eigen::MatrixXf Tensor2d;
-typedef std::pair <size_t, size_t> Shape;
-typedef std::tuple <size_t, size_t, size_t, string, float, float> Metadata; 
+typedef std::pair<size_t, size_t> Shape;
+typedef std::tuple<size_t, size_t, size_t, string, float, float> Metadata;
 
-class Dense 
+class Dense : public Layer
 {
-	public:
-		Dense(uint neurons, const string& afunc = "sigmoid", float lr = 1e-3, float er = 1e-8)  
-		{
-			meta = std::make_tuple(neurons, 0, 0, afunc, lr, er);
-		}
+public:
+	Dense(uint neurons, const string &afunc = "sigmoid",
+		  float lr = 1e-3, float er = 1e-8)
+	{
+	}
 
-		void init(const Shape &dim)
-		{
-			
-		}
+	void init(const Shape &dim)
+	{
+	}
 
-		void update()
-		{
+	void ToHost()
+	{
+	}
 
-		}
+	void ToDevice()
+	{
+	}
 
-	private:
-		float vu_, vb_;
-		Tensor2d w_, b_, 
-				 h_, dh_, delta_;
-		
-		Metadata meta;
+	void update();
+	void backward();
+	void forward(const Tensor2d &X);
 
+	float MSELoss(Matrix &Y, float &accuracy);
+	float CrossEntropyLoss(Matrix &Y, float &accuracy);
+
+private:
+	float vu_, vb_;
+	Matrix W_, B_, H_, dH_, lgrad_;
+	std::function<Matrix &, Matrix &, float> func;
+
+	void init_weight()
+	{
+		B_.Constant(1.0);
+		W_.Uniform(-0.2, 0.2);
+	}
 };
+
+void Dense::forward(const Tensor2d &X)
+{
+}
+
+void Dense::backward()
+{
+}
+
+void Dense::update()
+{
+}
+
+float Dense::MSELoss(Matrix &Y, float &accuracy)
+{
+}
+
+float Dense::CrossEntropyLoss(Matrix &Y, float &accuracy)
+{
+}
 
 #endif
