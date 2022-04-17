f209ec7a726bcd811e34eafd07a6151e1bb08535
Bao Pham <baopham4998@gmail.com>
2022-04-17 15:37:53 -0400

    change

--- src/dense.h
+++ src/dense.h
@@ -10,11 +10,22 @@ typedef std::pair<size_t, size_t> Shape;
 class Dense : public Layer
 {
 public:
-	Dense();
+	Dense() : W_(NULL), B_(NULL), H_(NULL), dH_(NULL), lgrad_(NULL)
+	{
+		vw_ = 0.0f;
+		vb_ = 0.0f;
+		out_dim = std::make_pair(0, 0);
+	}
 
 	Dense(uint neurons,
 		  const string &afunc = "sigmoid",
-		  float lr = 1e-3, float er = 1e-8);
+		  float lr = 1e-3, float er = 1e-8)
+	{
+		Dense();
+		lr_ = lr;
+		er_ = er;
+		afunc_ = afunc;
+	}
 
 	void init(int in_dim)
 	{
@@ -56,12 +67,49 @@ public:
 		return dH_;
 	}
 
-	void update();
-	void forward(const Matrix &X);
-	void set_delta(const Matrix &delta);
+	void forward(const Matrix &X)
+	{
+		H_ = W_.transpose() % X + B_;
+		func_(H_, dH_);
+		I_ = X;
+	}
 
-	float MSELoss(Matrix &Y, float &accuracy) override;
-	float CrossEntropyLoss(Matrix &Y, float &accuracy) override;
+	void update()
+	{
+		static Matrix dW;
+		I_.T();
+		dW = dH_;
+		dW.dot(I_);
+		dW.pow(2.0);
+
+		// adam parameters
+		vw_ = 0.1 * vw_ + 0.9 * dW.sum();
+		vb_ = 0.1 * vb_ + 0.9 * (dH_ * dH_).sum();
+		lgrad_ = W_.transpose() % dH_;
+	}
+
+	void set_delta(const Matrix &delta);
+	{
+		dH_ *= delta;
+	}
+
+	float MSELoss(const Matrix &Y, float &accuracy) override
+	{
+		dH_ = dH_ * (H_ - Y);
+		return sqrtf((H_ - Y).power(2.0).sum());
+	}
+
+	float CrossEntropyLoss(const Matrix &Y, float &accuracy) override
+	{
+		dH_ = H_ - Y;
+
+		Matrix J = H_;
+		J.Log();
+		J *= -1.0;
+		J *= Y;
+
+		return J.sum();
+	}
 
 private:
 	Shape out_dim;
@@ -87,65 +135,5 @@ private:
 			mat.ToHost();
 	}
 };
-
-Dense::Dense() : W_(NULL), B_(NULL), H_(NULL), dH_(NULL), lgrad_(NULL)
-{
-	vw_ = 0.0f;
-	vb_ = 0.0f;
-	out_dim = std::make_pair(0, 0);
-}
-
-Dense::Dense(uint neurons, const string &afunc = "sigmoid",
-			 float lr = 1e-3, float er = 1e-8)
-{
-	Dense();
-	lr_ = lr;
-	er_ = er;
-	afunc_ = afunc;
-}
-
-void Dense::forward(const Matrix &X)
-{
-	H_ = W_.transpose() % X + B_;
-	func_(H_, dH_);
-	I_ = X;
-}
-
-void Dense::update()
-{
-	static Matrix dW;
-	I_.T();
-	dW = dH_;
-	dW.dot(I_);
-	dW.pow(2.0);
-
-	// adam parameters
-	vw_ = 0.1 * vw_ + 0.9 * dW.sum();
-	vb_ = 0.1 * vb_ + 0.9 * (dH_ * dH_).sum();
-	lgrad_ = W_.transpose() % dH_;
-}
-
-void Dense::set_delta(const Matrix &delta)
-{
-	dH_ *= delta;
-}
-
-float Dense::MSELoss(const Matrix &Y, float &accuracy) override
-{
-	dH_ = dH_ * (H_ - Y);
-	return sqrtf((H_ - Y).power(2.0).sum());
-}
-
-float Dense::CrossEntropyLoss(const Matrix &Y, float &accuracy) override
-{
-	dH_ = H_ - Y;
-
-	Matrix J = H_;
-	J.Log();
-	J *= -1.0;
-	J *= Y;
-
-	return J.sum();
-}
 
 #endif
