d707d0eb0dc5240dbc00872bc77ca63af5e248c0
Crotty <PCPBcrtt@dcsfen01.ccni.rpi.edu>
2022-04-25 20:32:21 -0400

    parallel SGD working

--- mpi/mpi_driver.cpp
+++ mpi/mpi_driver.cpp
@@ -3,19 +3,26 @@
 #include <iostream>
 #include <cstdlib>
 #include <string.h>
+#include <vector>
+#include <numeric>
+#include <random>
+#include <algorithm>
+#include "../src/clockcycle.h"
 
 extern int init_cuda(int my_rank);
 extern void create_network(int input_size, int num_hidden, int* hidden_sizes, int output_size, std::string activation);
-extern void load_train_test(int input_size, int output_size);
-extern void run_forward_prop();
-extern float run_back_prop();
-extern void update_network();
+extern int load_train_test(int input_size, int output_size, int batch_size);
+extern void run_forward_prop(int batch_id);
+extern void update_network(float* grad);
 extern float test_network();
 extern void free_network();
 extern float* get_gradient();
-extern void set_gradient(float* grad);
-extern float* get_yh();
-extern float get_loss(float* yh);
+extern std::vector<float*> get_yh();
+extern float get_loss();
+extern void train_network(int batches);
+extern void train_network(int batches, int* batch_idx);
+extern float get_acc();
+extern std::pair<float, float> run_loss(std::vector<float*> yh);
 
 bool print_usage(){
     std::cerr << "USAGE:\n mpi_network <Input Dim> <# hidden layers> <layer sizes...> <Output Dim> <Epochs> [--optional] \n"
@@ -34,34 +41,44 @@ bool print_usage(){
     return false;
 }
 
-void read_options(int argc, char** argv, int offset, bool& mode, std::string& activation){
+void read_options(int argc, char** argv, int offset, bool& mode, std::string& activation, int& batch_size){
     if(strcmp(argv[5+offset], "--mode") == 0){
-    if(strcmp(argv[6+offset], "averaged") == 0){
-         mode = 1;
-    }
+        if(strcmp(argv[6+offset], "averaged") == 0){
+            mode = 1;
+        }
     }
     else if(strcmp(argv[5+offset], "--activation") == 0){
-    if(strcmp(argv[6+offset], "tanh")==0) activation = "tanh";
-    else if(strcmp(argv[6+offset], "sigmoid") == 0) activation = "sigmoid";
-    else if(strcmp(argv[6+offset], "ELU") == 0) activation = "ELU";
-    else if(strcmp(argv[6+offset], "sign") == 0) activation = "sign";
-    else if(strcmp(argv[6+offset],"identity") == 0) activation = "identity";
-    else if(strcmp(argv[6+offset], "softmax") == 0) activation = "softmax";
-    else activation = "ReLU";
+        if(strcmp(argv[6+offset], "tanh")==0)
+           activation = "tanh";
+        else if(strcmp(argv[6+offset], "sigmoid") == 0)
+            activation = "sigmoid";
+        else if(strcmp(argv[6+offset], "ELU") == 0)
+            activation = "ELU";
+        else if(strcmp(argv[6+offset], "sign") == 0)
+            activation = "sign";
+        else if(strcmp(argv[6+offset],"identity") == 0)
+            activation = "identity";
+        else if(strcmp(argv[6+offset], "softmax") == 0)
+            activation = "softmax";
+        else
+            activation = "ReLU";
+    }
+    else if(strcmp(argv[5+offset], "--batch_size") == 0){
+        batch_size = atoi(argv[6+offset]);
     }
 }
 
-bool read_parameters(int argc, char** argv, int& input_size, int& num_layers, int*& layer_sizes, int& output_size, int& epochs, bool& mode, std::string& activation){
+bool read_parameters(int argc, char** argv, int& input_size, int& num_layers, int*& layer_sizes, int& output_size, int& epochs, bool& mode, std::string& activation, int& batch_size){
     if(argc < 5)
-    return print_usage();
+        return print_usage();
     
     input_size = atoi(argv[1]);
     if(input_size == 0)
-    return print_usage();
+        return print_usage();
 
     num_layers = atoi(argv[2]);
     if(num_layers == 0)
-    return print_usage();
+        return print_usage();
     
     layer_sizes = (int*)malloc(num_layers * sizeof(int));
     for(int i = 0; i < num_layers; i++){
@@ -79,10 +96,12 @@ bool read_parameters(int argc, char** argv, int& input_size, int& num_layers, in
     return print_usage();
 
     if(argc > 5 + num_layers)
-    read_options(argc, argv, num_layers, mode, activation);
+        read_options(argc, argv, num_layers, mode, activation, batch_size);
     if(argc > 7 + num_layers)
-    read_options(argc, argv, num_layers+2, mode, activation);
-
+        read_options(argc, argv, num_layers+2, mode, activation, batch_size);
+    if(argc > 9 + num_layers)
+        read_options(argc, argv, num_layers+4, mode, activation, batch_size);
+ 
     return true;
 }
 
@@ -94,12 +113,18 @@ int main(int argc, char** argv){
     int output_size;
     int epochs;
     bool mode = 0;
+    int batch_size = 512;
+    int batches_per_update = 1;
     std::string activation = "ReLU";
-    int valid = read_parameters(argc, argv, input_size, num_layers, layer_sizes, output_size, epochs, mode, activation);
+    int valid = read_parameters(argc, argv, input_size, num_layers, layer_sizes, output_size, epochs, mode, activation, batch_size);
     if(!valid){ return 1;}
 
-    int test_size = 100;
     float loss = 0.0;
+    float acc = 0.0;
+
+    unsigned long long start_time;
+    unsigned long long end_time;
+    int clock_frequency = 512000000;
     
     int my_rank;
     int world_size;
@@ -115,67 +140,131 @@ int main(int argc, char** argv){
 
     // Instatiate the networks
     create_network(input_size, num_layers, layer_sizes, output_size, activation);
-    load_train_test(input_size, output_size);
+    int num_batches = load_train_test(input_size, output_size, batch_size);
 
     // Averaged Distributed Network
     if(mode){
-    // For each epoch
+    // For each epoc
+        start_time = clock_now();
         for(int epoch = 0; epoch < epochs; epoch++){
-            // Run forward propogation on each rank
-            run_forward_prop();
-            // Calculate gradients with backprop
-            loss = run_back_prop();
-            if(my_rank == 0)
-                std::cout << "Loss: " << loss << std::endl;
-            // Retrive gradients;
-            float* grad = get_gradient();
-            float* res_grad;
-            // Average gradients accross all processes
-            MPI_Allreduce(&grad, &res_grad, output_size, MPI_FLOAT, MPI_SUM, MPI_COMM_WORLD);
-            for(int i = 0; i < world_size; i++)
-                res_grad[i] /= world_size;
-            // Pass gradients back to NN
-            set_gradient(res_grad);
-            // Update networks with the set gradient
-            update_network();
+            std::vector<int> indices(num_batches);
+            int* indices_raw;
+            
+            indices_raw = (int*)malloc(sizeof(int)*num_batches); 
+            if(my_rank == 0){
+                std::iota (indices.begin(), indices.end(), 0);
+                auto rng = std::default_random_engine {};
+                std::shuffle(indices.begin(), indices.end(), rng);
+                for(int i = 0; i< num_batches; i++)
+                    indices_raw[i] = indices[i];
+            }
+            MPI_Barrier(MPI_COMM_WORLD);
+            MPI_Bcast(indices_raw, num_batches, MPI_INT, 0, MPI_COMM_WORLD);
+            MPI_Barrier(MPI_COMM_WORLD);
+
+            for(int batches = 0; batches < 4 * num_batches/world_size; batches++){
+                //for(int q = 0; q < 1; q++){
+                //    if(batches + 1 < num_batches){
+                //        run_forward_prop(indices_raw[batches]);
+                //        update_network(get_gradient());
+                //        batches++;
+                //    }
+                //    else{break;}
+                //}    
+                run_forward_prop(indices_raw[(my_rank + (batches*world_size)) % num_batches]);
+                loss = get_loss();
+                acc = get_acc();
+                // Retrive gradients;
+                float* grad = get_gradient();
+                float* res_grad = (float*)malloc(sizeof(float)* batch_size * output_size);
+                // Average gradients accross all processes
+                MPI_Allreduce(grad, res_grad, batch_size * output_size, MPI_FLOAT, MPI_SUM, MPI_COMM_WORLD);
+                for(int i = 0; i < batch_size * output_size; i++)
+                    res_grad[i] /= world_size;
+                // Pass gradients back to NN
+                update_network(res_grad);
+                // Update networks with the set gradient
+            }
+            loss = get_loss();
+            acc = get_acc();
+            float avg_loss = 0.0;
+            MPI_Reduce(&loss, &avg_loss, 1, MPI_FLOAT, MPI_SUM, 0, MPI_COMM_WORLD);
+            float avg_acc = 0.0;
+            MPI_Reduce(&acc, &avg_acc, 1, MPI_FLOAT, MPI_SUM, 0, MPI_COMM_WORLD);
+            if(my_rank == 0){
+                end_time = clock_now();
+                //std::cout << tworks: " << avg_loss/world_size << std::endl;
+                std::cout << avg_acc/world_size << std::endl;
+                //double time_in_secs = ((double)(end_time - start_time)) / clock_frequency;
+                //std::cout << "Time: " << time_in_secs << std::endl;
+            }
+            free(indices_raw);
         }
         if(my_rank == 0){
+            end_time = clock_now();
             loss = test_network();
+            acc = get_acc();
             std::cout << "Averaged Network Loss: " << loss << std::endl;
+            std::cout << "Averaged Network Acc: " << acc << std::endl;
+            double time_in_secs = ((double)(end_time - start_time)) / clock_frequency;
+            std::cout << "Time: " << time_in_secs << std::endl;
+
         }
     }
     // Ensemble learning 
     else{
         // For each epoch
+        start_time = clock_now();
         for(int epoch = 0; epoch < epochs; epoch++){
-            // Run forward propogation on each rank
-            run_forward_prop();
-            // Run Backward propogation on each rank
-            loss = run_back_prop();
-            std::cout << "Rank: " << my_rank << " loss: " << loss << std::endl;
-            // Update each network
-            update_network();
+            // Run all batches
+            train_network(num_batches);
+            // Get the loss 
+            loss = get_loss();
+            acc = get_acc();
+            std::cout << "Rank: " << my_rank << " epoch: " << epoch << " loss: " << loss << " acc: " << acc << std::endl;
+            //update_network();
+            
         }
+        MPI_Barrier(MPI_COMM_WORLD);
         // Print out the average loss of all networks
         float loss = test_network();
         float avg_loss = 0.0;
         MPI_Reduce(&loss, &avg_loss, 1, MPI_FLOAT, MPI_SUM, 0, MPI_COMM_WORLD);
+        float acc = get_acc();
+        float avg_acc = 0.0;
+        MPI_Reduce(&acc, &avg_acc, 1, MPI_FLOAT, MPI_SUM, 0, MPI_COMM_WORLD);
+        end_time = clock_now();
+        std::cout << "Rank: " << my_rank << " Final Loss: " << loss << " Final Acc: " << acc << std::endl;
         if(my_rank == 0){
             std::cout << "Average Loss of all networks: " << avg_loss/world_size << std::endl;
+            std::cout << "Average Accuracy of all networks: "  << avg_acc/world_size << std::endl;
+            double time_in_secs = ((double)(end_time - start_time)) / clock_frequency;
+            std::cout << "Time: " << time_in_secs << std::endl;
         }
-        
-        float* yh = get_yh();
-        float* avg_yh = (float*)malloc(test_size * sizeof(float));
-        MPI_Reduce(&yh, &avg_yh, test_size, MPI_FLOAT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+        std::vector<float*> yh = get_yh();
+        std::vector<float*> avg_yh;
+        MPI_Barrier(MPI_COMM_WORLD);
+        float* res_yh = (float*)malloc(batch_size * sizeof(float));
+
+        for(int i = 0; i < yh.size(); i++){
+            MPI_Reduce(yh[i], res_yh, batch_size, MPI_FLOAT, MPI_SUM, 0, MPI_COMM_WORLD);
+            MPI_Barrier(MPI_COMM_WORLD);
+            avg_yh.push_back(res_yh);
+        }
+        free(res_yh);
+
         if(my_rank == 0){
-            for(int i = 0; i < test_size; i++)
-                avg_yh[i] /= world_size;
-            loss = get_loss(avg_yh);
-            std::cout << "Loss of averaged output of networks: " << loss << std::endl;
+            for(int i = 0; i < avg_yh.size(); i++){
+                for(int j = 0; j < batch_size; j++)
+                    avg_yh[i][j] /= (float)world_size;
+            }
+            std::pair<float, float> res = run_loss(avg_yh);
+            std::cout << "Loss of averaged output of networks: " << res.first << std::endl;
+            std::cout << "Acc of averaged output of networks: " << res.second << std::endl;
         }
     }
-    
-    free_network();
+    MPI_Barrier(MPI_COMM_WORLD); 
 
     MPI_Finalize();
 }
--- mpi/neural_net.cu
+++ mpi/neural_net.cu
@@ -1,8 +1,8 @@
 #include "../src/modules.h"
 #include "../src/dense.h"
 #include "../src/cu_mat.h"
-#include "../src/clockcycle.h"
 #include "../src/utils.h"
+#include "../src/parse_mnist.h"
 #include <vector>
 #define L std::unique_ptr<Layer>
 
@@ -10,7 +10,10 @@
 std::vector<L> network;
 int layers, out_dim;
 Matrix X, Y, dJ;
-float loss, acc;
+float loss, acc, lr;
+std::vector<Matrix> train_images, train_labels;
+std::vector<Matrix> test_images, test_labels;
+std::vector<float*> yh;
 
 extern int init_cuda(int my_rank){
     int cudaDeviceCount = 0;
@@ -28,76 +31,147 @@ extern int init_cuda(int my_rank){
 }
 
 
-extern void load_train_test(int input_size, int output_size){
-    X = Matrix(100, input_size);
-    X.ToDevice();
-    X.Uniform(-1, 1);
+extern int load_train_test(int input_size, int output_size, int batch_size){
+   
+    load_mnist("../data/train-images-idx3-ubyte", "../data/train-labels-idx1-ubyte", batch_size, train_images, train_labels);
+    load_mnist("../data/t10k-images-idx3-ubyte", "../data/t10k-labels-idx1-ubyte", batch_size, test_images, test_labels);
     
-    Y = Matrix(100, output_size);
-    Y.ToDevice();
-    Y.Uniform(0, 1);
+    //std::cout << "Loaded " << train_images.size() << " train images" << std::endl;
+    //std::cout << "Loaded " << test_images.size() << " test images" << std::endl;    
+    //std::cout << train_images[0].shape().first << ", " << train_images[0].shape().second << std::endl;
+    
+    return train_images.size();
 }
 
 extern void create_network(int input_size, int num_hidden, int* hidden_sizes, int output_size, std::string activation){
-    std::cout << "creating network" << std::endl;
+    //std::cout << "creating network" << std::endl;
     layers = 1 + num_hidden;
     out_dim = output_size;
     dJ = Matrix(output_size, 1);
     network = std::vector<L>(layers);
     loss = 0.0;
     acc = 0.0;
+    lr = 0.001;
     
-    network[0] = L{new Dense(hidden_sizes[0], activation)};
+    network[0] = L{new Dense(hidden_sizes[0], activation, lr)};
     network[0]->init(input_size);
     
     for(int i = 1; i < num_hidden; i++){
-        network[i] = L{new Dense(hidden_sizes[i], activation)};
+        network[i] = L{new Dense(hidden_sizes[i], activation, lr)};
         network[i] -> init(hidden_sizes[i-1]);
     }
     
-    network[layers-1] = L{new Dense(output_size, activation)};
+    network[layers-1] = L{new Dense(output_size, activation, lr)};
     network[layers-1]->init(hidden_sizes[num_hidden-1]);
     
+}
+
+extern void train_network(int num_batches){
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distrib(0, train_images.size()-1);
+    loss = 0.0;
+    acc = 0.0;
+    float ret_acc;
+    for(int batch = 0; batch < num_batches; batch++){
+        uint batchid = distrib(gen); 
+        network[0]->forward(train_images[batchid]);
+        for(int i = 1; i < layers; i++){
+            network[i]->forward(network[i-1]->Get_H());
+        }
+        loss += network.back()->CrossEntropyLoss(train_labels[batchid], ret_acc);
+        // loss += network.back()->MSELoss(train_labels[batchid], ret_acc);
+
+        acc += ret_acc;
+        network.back()->update();
+        for(int i = layers-2; i >= 0; i--){
+            network[i]->set_delta(network[i+1]->Get_delta());
+            network[i]->update();
+        }
+    }
     for(int i = 0; i < layers; i++)
-        network[i]->ToDevice();
+        network[i]->set_lr(network[i]->get_lr() * 0.9);
+    loss /= num_batches;
+    acc /= num_batches;
 }
 
-extern void run_forward_prop(){
-    std::cout << "running forward prop" << std::endl;
-    network[0]->forward(X);
+extern void train_network(int num_batches, int* batches){
+    loss = 0.0;
+    acc = 0.0;
+    float ret_acc;
+    for(int batch = 0; batch < num_batches; batch++){
+        network[0]->forward(train_images[batches[batch]]);
+        for(int i = 1; i < layers; i++)
+            network[i]->forward(network[i-1]->Get_H());
+        loss += network.back()->CrossEntropyLoss(train_labels[batches[batch]], ret_acc);
+        //loss += network.back()->CrossEntropyLoss(train_labels[batches[batch]], ret_acc);
+
+        acc += ret_acc;
+        network.back()->update();
+        for(int i = layers-2; i >= 0; i--){
+            network[i]->set_delta(network[i+1]->Get_delta());
+            network[i]->update();
+        }
+    }
+    acc /= num_batches;
+    loss /= num_batches;
+}
+
+extern void run_forward_prop(int batch_id){
+    //std::cout << "running forward prop" << std::endl;
+    //std::cout << batch_id << std::endl;
+    loss = 0.0;
+    network[0]->forward(train_images[batch_id]);
     for(int i = 1; i < layers; i++){
-        network[i]->forward(network[i-1]->get_H());    
+        network[i]->forward(network[i-1]->Get_H());    
     }
+    loss += network.back()->CrossEntropyLoss(train_labels[batch_id], acc);
+    //loss += network.back()->MSELoss(train_labels[batch_id], acc);
+
+    //loss /= 2;
+ 
 }
 
-extern float run_back_prop(){
-    std::cout << "running backward prop" << std::endl;
-    std::cout << network.back()->OutShape() << std::endl;
-    loss = network.back()->CrossEntropyLoss(Y, acc);
-    std::cout << "AHHHHHHHHH" << std::endl;
-    network.back()->get_dJ();
-    std::cout << "done back prop" << std::endl;
-    return loss;
-}
-
-extern void update_network(){
-    std::cout << "updating network" << std::endl;
-    
+extern void update_network(float* grad){
+    //std::cout << "updating network" << std::endl;
+    //std::cout << dJ.shape() << std::endl;
+    dJ = Matrix(train_images[0].shape().first, out_dim, grad);
+    //std::cout << dJ.shape() << std::endl;
     network.back()->set_dJ(dJ);
-    std::cout << "1\n";
     network.back()->update();
-    std::cout << "2\n";
     for(int i = layers-2; i >= 0; i--){
-        network[i]->set_delta(network[i+1]->get_delta());
-        std::cout << "3\n";
+        network[i]->set_delta(network[i+1]->Get_delta());
         network[i]->update();
-        std::cout << "4\n";
     }
+    for(int i = 0; i < layers; i++)
+        network[i]->set_lr(network[i]->get_lr() * 0.9999);
+    // std::cout << "done\n";
 }
 
 
-extern void test_network(){
+extern float test_network(){
     std::cout << "testing network" << std::endl;
+    float test_loss = 0.0;
+    yh.clear();
+    acc = 0.0;
+    float ret_acc;
+    for(int b = 0; b < test_images.size(); b++){
+        network[0]->forward(test_images[b]);
+        for(int i = 1; i < layers; i++)
+            network[i]->forward(network[i-1]->Get_H());
+        //test_loss += network.back()->MSELossNoGrad(test_labels[b], ret_acc);
+        Matrix h = network.back()->Get_H().bin();
+        test_loss += network.back()->CrossEntropyLossNoGrad(test_labels[b], ret_acc);
+        //test_loss += network.back()->BCELossNoGrad(test_labels[b], ret_acc);
+
+        acc += ret_acc;
+        // Matrix h = network.back()->Get_H().bin();
+        h.ToHost();
+        //h = h.bin();
+        yh.push_back((float*)(h.HostData().data()));
+    }
+    acc /= test_images.size();
+    return test_loss / test_images.size();
 }
 
 extern void free_network(){
@@ -105,23 +179,37 @@ extern void free_network(){
 }
 
 extern float* get_gradient(){
-    dJ.ToDevice();
-    dJ = network.back()->get_dJ();
+    dJ = network.back()->Get_dJ();
     dJ.ToHost();
     return (float*)dJ.HostData().data();
 }
 
-extern void set_gradient(float* grad){
-    dJ.ToHost();
-    dJ = Matrix(out_dim, 1, grad);
-    dJ.ToDevice();
+extern std::vector<float*> get_yh(){
+    return yh;
 }
 
-extern float* get_yh(){
-    float* q = (float*)malloc(100 * sizeof(float));
-    return q;
+extern float get_loss(){
+    return loss;
 }
 
-extern float get_loss(float* yh){
-    return 0.0;
+extern float get_acc(){
+    return acc;
 }
+
+extern std::pair<float, float> run_loss(std::vector<float*> avg_yh){
+    Matrix my_h;
+    //test_labels[0].ToHost();
+    //std::cout << std::endl << test_labels[0] << std::endl;
+    float avg_loss = 0.0;
+    float avg_acc = 0.0;
+    for(int i = 0; i < test_labels.size(); i++){
+        test_labels[i].ToHost();
+        my_h.ToHost();
+        my_h = Matrix(test_labels[i].size(), 1, avg_yh[i]);
+        my_h.ToHost(); 
+        avg_acc += my_h.bin().compare(test_labels[i]) / test_labels[i].shape().first;
+        avg_loss += ((-1.0 * test_labels[i]) * my_h.log()).sum() / test_labels[i].shape().first;
+        //avg_loss += sqrtf((my_h - test_labels[i]).pow(2.0).sum()) / test_labels[i].shape().first; 
+    }
+    return std::make_pair(avg_loss/avg_yh.size(), avg_acc/avg_yh.size());
+} 
